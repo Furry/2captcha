2279d5c1b3b1bd9ffb3364194372a6af9ce7961c
Furry <ender@thefluegges.com>
2022-03-18 10:37:24 -0400

    test

--- tests/promiseTest.js
+++ tests/promiseTest.js
@@ -1,3 +1,11 @@
 const promiseMap = {};
 
-promiseMap["test1"] = new Promise(())
+promiseMap["test1"] = new Promise(() => {});
+
+promiseMap["test1"].then((data) => {
+    console.log(`Completed! Data: ${data}`);
+});
+
+setTimeout(() => {
+    promiseMap["test1"].resolve("Hello, world uwu");
+}, 5000);
