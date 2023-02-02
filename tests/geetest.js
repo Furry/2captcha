9c410685a07743db5617167fe6d1fdcb8e4f72ad
unknown <dzmitry.duboyski@gmail.com>
2023-02-03 00:30:17 +0400

    refactor: update demo for geetest captcha

--- tests/geetest.js
+++ tests/geetest.js
@@ -1,5 +1,7 @@
 const Captcha = require("../dist/index.js");
-const Solver = new Captcha.Solver(process.argv[2], 1000);
+require('dotenv').config();
+const APIKEY = process.env.APIKEY
+const Solver = new Captcha.Solver(APIKEY);
 
 Solver.geetest("d7e36de8f91fae3768e8f4fadfa3bf1f", "2bc0c39f85b8c44acb70971f5581e38e", "https://marketplace.axieinfinity.com/profile/dashboard")
 .then((res) => {
