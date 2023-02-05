5c42aab8990f032574e9ab68b155eec5a9d76bfe
unknown <dzmitry.duboyski@gmail.com>
2023-02-05 21:55:18 +0400

    reractor: update example

--- tests/recaptcha.js
+++ tests/recaptcha.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 const APIKEY = process.env.APIKEY
 const solver = new Captcha.Solver(APIKEY);
 
-solver.recaptcha("6Ld2sf4SAAAAAKSgzs0Q13IZhY02Pyo31S2jgOB5", "https://patrickhlauke.github.io/recaptcha/")
+solver.recaptcha({
+    pageurl: 'https://2captcha.com/demo/recaptcha-v2',
+    googlekey: '6LfD3PIbAAAAAJs_eEHvoOl75_83eXSqpPSRFJ_u'
+})
 .then((res) => {
     console.log(res);
 })
