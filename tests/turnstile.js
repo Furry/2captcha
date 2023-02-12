798cc410adbe62dcdf5346f11004dc5427016b7f
unknown <dzmitry.duboyski@gmail.com>
2023-02-12 18:42:48 +0400

    feat: add example for Cloudflare Turnstile captcha

---
+++ tests/turnstile.js
@@ -0,0 +1,15 @@
+const Captcha = require("../dist/index.js");
+require('dotenv').config();
+const APIKEY = process.env.APIKEY
+const solver = new Captcha.Solver(APIKEY);
+
+solver.сloudflareTurnstile({
+    pageurl: "https://app.nodecraft.com/login",
+    sitekey: "0x4AAAAAAAAkg0s3VIOD10y4"    
+})
+.then((res) => {
+    console.log(res);
+})
+.catch((err) => {
+    console.log(err);
+})
