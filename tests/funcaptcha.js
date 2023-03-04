29111aee75146a791f401d57bb59fa215549b0c3
unknown <dzmitry.duboyski@gmail.com>
2023-03-04 17:47:59 +0400

    refactor: update funcaptcha code example

--- tests/funcaptcha.js
+++ tests/funcaptcha.js
@@ -5,7 +5,8 @@ const solver = new Captcha.Solver(APIKEY);
 
 solver.funCaptcha({
     pageurl: "https://api.funcaptcha.com/fc/api/nojs/?pkey=69A21A01-CC7B-B9C6-0F9A-E7FA06677FFC",
-    publickey: "69A21A01-CC7B-B9C6-0F9A-E7FA06677FFC"
+    publickey: "69A21A01-CC7B-B9C6-0F9A-E7FA06677FFC",
+    data: '{"blob": "foo"}'
 })
 .then((res) => {
     console.log(res);
