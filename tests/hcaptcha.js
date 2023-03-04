684eab602efa37cf996afe886e8e976aef59df41
unknown <dzmitry.duboyski@gmail.com>
2023-03-04 16:43:17 +0400

    refactor: update code example for hCaptcha

--- tests/hcaptcha.js
+++ tests/hcaptcha.js
@@ -4,7 +4,7 @@ const APIKEY = process.env.APIKEY
 const solver = new Captcha.Solver(APIKEY);
 
 solver.hcaptcha({
-    pageurl: "https://2captcha.com/demo/hcaptcha?difficulty=moderate",
+    pageurl: "https://2captcha.com/demo/hcaptcha",
     sitekey: "b76cd927-d266-4cfb-a328-3b03ae07ded6"
 })
 .then((res) => {
